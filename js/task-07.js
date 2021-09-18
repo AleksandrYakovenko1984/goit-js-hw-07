54e43b855a6513ba5bd16618648329d7d21c6ba1
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2021-09-18 09:57:36 +0300

    7-task

--- js/task-07.js
+++ js/task-07.js
@@ -7,10 +7,11 @@ spanLink: document.querySelector("#text")
 refs.inputLink.addEventListener('input', onInputChange)
 
 function onInputChange(event) {
-    event.target === '50px'
+    
     console.log(event.currentTarget.value);
     if (event.target !== event.currentTarget.value) {
-       refs.spanLink.textContent === event.currentTarget.value
+        refs.spanLink.setAttribute(
+   "style", `font-size: ${event.currentTarget.value}px`);
    }
     
    
